aeeea595c133b14330af420984c7cc26776fafe0
itgsod-emre-doner <emre.doner@elev.it-gymnasiet.se>
2014-06-03 08:49:43 +0200

    Ändringar på koden och tillägg
    Ändrade till så att list(cleartext) har någonstans att hamna och la
    till så att vi kan få karaktär utav listan. Hade en onödig mening
    "cleartext = raw_input('What do you want to encrypt?')" så jag tog bort
    den.

--- lib/encryption.py
+++ lib/encryption.py
@@ -5,10 +5,9 @@ def encrypt(cleartext, offset):
         raise ValueError('offset must not be zero')
 
 
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
+    cleartext = cleartext.upper()
+    cleartext = list(cleartext)
+    for letter in cleartext:
+        print alphabet.index(letter)
     return ""
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
-cleartext = raw_input('What do you want to encrypt?')
-cleartext = cleartext.upper()
-list(cleartext)
-offset = raw.input('What is offset?')
-
